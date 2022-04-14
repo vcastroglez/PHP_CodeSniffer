5bd444ee128d8c1d4072925aa325890ce3dcd3c2
Vladimir Castro <vcastroglez@gmail.com>
2022-04-14 13:40:38 +0300

    not cammel!!!

--- src/Util/Common.php
+++ src/Util/Common.php
@@ -515,6 +515,42 @@ class Common
         return $validName;
 
     }//end isUnderscoreName()
+    /**
+     * Returns true if the specified string is in the underscore caps format.
+     *
+     * @param string $string The string to verify.
+     *
+     * @return boolean
+     */
+    public static function isSnakeName($string)
+    {
+        // If there are space in the name, it can't be valid.
+        if (strpos($string, ' ') !== false) {
+            return false;
+        }
+
+        $validName = true;
+        $nameBits  = explode('_', $string);
+
+        if (preg_match('|^[a-z]|', $string) === 0) {
+            // Name does not begin with a not capital letter.
+            $validName = false;
+        } else {
+            foreach ($nameBits as $bit) {
+                if ($bit === '') {
+                    continue;
+                }
+
+                if ($bit[0] !== strtolower($bit[0])) {
+                    $validName = false;
+                    break;
+                }
+            }
+        }
+
+        return $validName;
+
+    }//end isUnderscoreName()
 
 
     /**
